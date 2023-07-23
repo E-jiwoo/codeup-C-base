548a922c97643dba252ec464ab2e10271e4664d4
E-jiwoo <livei07@naver.com>
2023-07-24 05:04:59 +0900

    1058

---
+++ 1058.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    scanf("%d", &a);
+    if(30<=a && a<=40){
+        printf("win");
+    }
+    else if (60<=a && a<=70){
+		printf("win");
+	}
+    else{
+        printf("lose");
+    }
+}

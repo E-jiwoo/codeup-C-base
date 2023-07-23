5ad7f068687a1f6c694fde077f3c813a61b51ded
E-jiwoo <livei07@naver.com>
2023-07-24 03:25:26 +0900

    1070

---
+++ 1070.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    scanf("%d", &a);
+    switch(a)
+    {
+    	case 3:
+    		printf("spring");
+    		break;
+    	case 4:
+    		printf("spring");
+    		break;
+		case 5:
+    		printf("spring");
+    		break;
+    	case 6:
+    		printf("summer");
+    		break;
+    	case 7:
+    		printf("summer");
+    		break;
+    	case 8:
+    		printf("summer");
+    		break;
+    	case 9:
+    		printf("fall");
+    		break;
+    	case 10:
+    		printf("fall");
+    		break;
+    	case 11:
+    		printf("fall");
+    		break;
+    	default:
+    		printf("winter");
+    		break;
+	}
+}
